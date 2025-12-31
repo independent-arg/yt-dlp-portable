135634845fe3289af28310df395f31cac7e36d00
independent-arg <brianedmanu@gmail.com>
2025-12-31 01:26:13 -0300

    Bump version to v0.6.1

--- download.sh
+++ download.sh
@@ -2,15 +2,15 @@
 
 # ==============================================================================
 # Script Name: yt-dlp-portable (download.sh)
-# Version:     v0.6.0-beta1
+# Version:     v0.6.1
 # Author:      independent-arg
 # License:     MIT
 # ==============================================================================
 
 set -euo pipefail
 
-readonly VERSION="v0.6.0-beta1"
-readonly LAST_UPDATED="2025-12-30"
+readonly VERSION="v0.6.1"
+readonly LAST_UPDATED="2025-12-31"
 
 # Function to handle interruptions (Ctrl+C)
 trap 'echo -e "\n${YELLOW}[INFO] Download interrupted by user.${NC}"; exit 130' INT
